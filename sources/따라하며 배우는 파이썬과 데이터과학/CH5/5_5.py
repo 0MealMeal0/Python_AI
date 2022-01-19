b196bdafee24ad76f4ec4f5a38b40aa725df4c1f
Seonhyun Kim <83529974+0MealMeal0@users.noreply.github.com>
2022-01-19 14:28:02 +0900

    Update 5_5.py

--- sources/따라하며 배우는 파이썬과 데이터과학/CH5/5_5.py
+++ sources/따라하며 배우는 파이썬과 데이터과학/CH5/5_5.py
@@ -2,8 +2,21 @@ depth = 30
 day = 1
 location = 0
 
-while(location <= 30): 
+while(True): 
 
-    for day in range(1, 14):
-        location += location + 2
-        print("day :  {} 달팽이의 위치 : {} 미터".format(day, location))
+    if location == 0 :
+        location = 7
+
+    else :
+        location = location + ((7 - 5))
+    
+    print("day :  {} 달팽이의 위치 : {} 미터".format(day, location))
+    
+    if(location >= 30) : 
+        print("축하합니다. 우물 탈출했습니다.")
+        print("우물 탈출에 {}일 걸렸습니다.".format(day))
+        break
+    
+    else:
+        day += 1
+
